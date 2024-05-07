8e2c6338ebe9d0ba716799790f7a24b08d479333
QAEZZ <russian.devv@gmail.com>
2024-05-07 08:24:47 -0500

    clear

--- src/components/WikiUser.tsx
+++
@@ -1,9 +0,0 @@
-import { createSignal } from "solid-js";
-
-export default function WikiUser(props: any) {
-    const name = props.name || "unknown-user";
-    const id = props.id || "";
-    return (
-        <p class="wiki-user" id={id}>{name}</p>
-    );
-}
--- src/routes/server/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Title } from "@solidjs/meta";
-import { useNavigate } from "@solidjs/router";
-
-export default function ServerHome() {
-
-    const navigate = useNavigate();
-    navigate('/');
-    return <></>;
-}

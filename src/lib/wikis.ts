3a76970b85c0f0c3bd140f3c075e2b5549f2e265
QAEZZ <russian.devv@gmail.com>
2024-05-08 13:27:39 -0500

    got started on server lookup

---
+++ src/lib/wikis.ts
@@ -0,0 +1,12 @@
+async function getWikiBySlug(id: string) { 
+
+}
+
+async function getAllWikiSlugs(): Promise<number[]> { // TODO: DONT MAKE ANY, THIS IS TEMP, MAKE A TYPE FOR THIS
+    return [12345, 54321, 6789, 9876];
+}
+
+export {
+    getWikiBySlug,
+    getAllWikiSlugs
+};
